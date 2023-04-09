04889f38302b93e4b5055a129068f16ed29f4229
Yulia Kostenko <y.kostenko81@gmail.com>
2023-04-10 00:14:17 +0300

    first method

--- Task/Program.cs
+++ Task/Program.cs
@@ -14,33 +14,3 @@ void Print1DArr(string[] arr)
     Console.WriteLine(arr[arr.Length - 1] + "]");
 }
 
-// Метод печати 
-int PrintData(string msg)
-{
-    Console.Write(msg);
-    int line = int.Parse(Console.ReadLine() ?? "0");
-    return line;
-}
-
-// Метод преобразования одномерного массива
-string[] TransArr(string[] arr)
-{
-    int len = arr.Length;
-    string[] resultArr = new string[len];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        int res = 0;
-        res = arr[i].Length;
-        if (res <= 3)
-        {
-            resultArr[i] = arr[i];
-        }
-    }
-    return resultArr;
-}
-
-string[] initialArr = { "1234", "1567", "-2", "computer science"};
-Print1DArr(initialArr);
-string[] resultArr = TransArr(initialArr);
-PrintData("Преобразованный массив:");
-Print1DArr(resultArr);
